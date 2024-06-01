449ab29676d3d0e247fca3dbe6b24e2915e0dd05
RodrigoA15 <rapapamija@misena.edu.co>
2024-06-01 10:19:13 -0500

    consultas para graficas

---
+++ src/controllers/chartsFilesController.ts
@@ -0,0 +1,100 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../db/connection";
+
+class ChartFilesController {
+  constructor() {}
+
+  async chartByDay(req: Request, res: Response) {
+    try {
+      const total = await AppDataSource.createQueryBuilder()
+        .select("EXTRACT(DAY FROM FECHA_PROCESO) AS DIA")
+        .addSelect("COUNT(*) AS TOTAL")
+        .from("ARCHIVO_SOLICITUD_PROCESO", "ARCHIVO_SOLICITUD_PROCESO")
+        .innerJoin(
+          "ARCHIVO_TIPO_DOCUMENTO",
+          "ARCHIVO_TIPO_DOCUMENTO",
+          "ARCHIVO_SOLICITUD_PROCESO.TIPO_DOC_ARCHIVO = ARCHIVO_TIPO_DOCUMENTO.ID_TIPO_DOC"
+        )
+        .innerJoin(
+          "QX_USUARIO",
+          "QX_USUARIO",
+          "ARCHIVO_SOLICITUD_PROCESO.ID_USUARIO = QX_USUARIO.ID_USUARIO_QX"
+        )
+        .innerJoin(
+          "ARCHIVO_MOTIVO",
+          "ARCHIVO_MOTIVO",
+          "ARCHIVO_SOLICITUD_PROCESO.ID_MOTIVO = ARCHIVO_MOTIVO.ID_MOTIVO"
+        )
+        .innerJoin(
+          "ARCHIVO_TIPO_ESTADO",
+          "ARCHIVO_TIPO_ESTADO",
+          "ARCHIVO_SOLICITUD_PROCESO.ID_PROCESO_ARCHIVO = ARCHIVO_TIPO_ESTADO.ID_ESTADO"
+        )
+        .where("ARCHIVO_SOLICITUD_PROCESO.ESTADO_ACTUAL =:ESTADO_ACTUAL", {
+          ESTADO_ACTUAL: "S",
+        })
+        .andWhere(
+          "TO_CHAR(FECHA_PROCESO, 'MM/YYYY') = TO_CHAR(SYSDATE , 'MM/YYYY')"
+        )
+        .groupBy("EXTRACT(DAY FROM FECHA_PROCESO)")
+        .orderBy("DIA")
+        .getRawMany();
+      if (total.length > 0) {
+        res.status(200).json(total);
+      } else {
+        res.status(404).json({ message: "Not found", TOTAL: 0 });
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(500).json(error.message);
+      }
+    }
+  }
+
+  async chartByMonth(req: Request, res: Response) {
+    try {
+      const total = await AppDataSource.createQueryBuilder()
+        .select("EXTRACT(MONTH FROM FECHA_PROCESO) AS MES")
+        .addSelect("COUNT(*) AS TOTAL")
+        .from("ARCHIVO_SOLICITUD_PROCESO", "ARCHIVO_SOLICITUD_PROCESO")
+        .innerJoin(
+          "ARCHIVO_TIPO_DOCUMENTO",
+          "ARCHIVO_TIPO_DOCUMENTO",
+          "ARCHIVO_SOLICITUD_PROCESO.TIPO_DOC_ARCHIVO = ARCHIVO_TIPO_DOCUMENTO.ID_TIPO_DOC"
+        )
+        .innerJoin(
+          "QX_USUARIO",
+          "QX_USUARIO",
+          "ARCHIVO_SOLICITUD_PROCESO.ID_USUARIO = QX_USUARIO.ID_USUARIO_QX"
+        )
+        .innerJoin(
+          "ARCHIVO_MOTIVO",
+          "ARCHIVO_MOTIVO",
+          "ARCHIVO_SOLICITUD_PROCESO.ID_MOTIVO = ARCHIVO_MOTIVO.ID_MOTIVO"
+        )
+        .innerJoin(
+          "ARCHIVO_TIPO_ESTADO",
+          "ARCHIVO_TIPO_ESTADO",
+          "ARCHIVO_SOLICITUD_PROCESO.ID_PROCESO_ARCHIVO = ARCHIVO_TIPO_ESTADO.ID_ESTADO"
+        )
+        .where("ARCHIVO_SOLICITUD_PROCESO.ESTADO_ACTUAL =:ESTADO_ACTUAL", {
+          ESTADO_ACTUAL: "S",
+        })
+        .andWhere("TO_CHAR(FECHA_PROCESO, 'YYYY') = TO_CHAR(SYSDATE , 'YYYY')")
+        .groupBy("EXTRACT(MONTH FROM FECHA_PROCESO)")
+        .orderBy("MES")
+        .getRawMany();
+      if (total.length > 0) {
+        res.status(200).json(total);
+      } else {
+        res.status(404).json({ message: "Not found", TOTAL: 0 });
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(500).json(error.message);
+      }
+    }
+  }
+}
+
+export default new ChartFilesController();
--- src/routes/archivoRoutes.ts
+++ src/routes/archivoRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import fileController from "../controllers/fileController";
 import userFilesController from "../controllers/userFilesController";
+import chartsFilesController from "../controllers/chartsFilesController";
 const router = Router();
 
 router.get("/day", fileController.documentsOutByDay);
@@ -10,5 +11,9 @@ router.get("/year", fileController.documentsOutByYear);
 //Documents by users
 router.get("/userDay", userFilesController.userDocumentsByDay);
 router.get("/userMonth", userFilesController.userDocumentsByMonth);
+
+//Charts by documents Out
+router.get("/chartDays", chartsFilesController.chartByDay);
+router.get("/chartMonth", chartsFilesController.chartByMonth);
 
 export default router;
