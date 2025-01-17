9cc4f8d22849d110b41e01a927b9b99719bacbf9
RodrigoA15 <rapapamija@misena.edu.co>
2025-01-17 17:19:24 -0500

    Add: variables  global

--- src/config.ts
+++ src/config.ts
@@ -7,3 +7,8 @@ export const PASSWORD = process.env.PASSWORD;
 export const HOSTNAME = process.env.HOSTNAME;
 export const SERVICE_NAME = process.env.SERVICE_NAME;
 export const MONGODB_URI = process.env.MONGODB_URI;
+//SAMBA OPTIONS
+export const ADDRES = process.env.ADDRESS;
+export const DOMAIN = process.env.DOMAIN;
+export const MAX_PROTOCOL = process.env.MAX_PROTOCOL;
+export const MASK_CMD = process.env.MASK_CMD;
