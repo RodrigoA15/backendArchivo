85365e89466e276df99ddd98fdbe96742d171ec3
RodrigoA15 <rapapamija@misena.edu.co>
2025-01-20 17:06:36 -0500

    Fix: return response  created lawyer

--- src/controllers/files/Lawyers/lawyers.controller.ts
+++ src/controllers/files/Lawyers/lawyers.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { Lawyers } from "../../../interfaces/lawyers.interface";
 import lawyerService from "../../../services/files/Lawyers/lawyers.service";
 import { CreateLawyerDto } from "../../../dtos/Lawyers.dto";
-
+import { HttpException } from "../../../exceptions/HttpException";
 class LawyersController {
   public lawyerService = new lawyerService();
 
@@ -18,7 +18,6 @@ class LawyersController {
       next(error);
     }
   };
-
   public createLawyer = async (
     req: Request,
     res: Response,
@@ -26,10 +25,8 @@ class LawyersController {
   ) => {
     try {
       const lawyerData: CreateLawyerDto = req.body;
-      const newLawyer: Lawyers = await this.lawyerService.createLawyer(
-        lawyerData
-      );
-      res.status(201).json({ data: newLawyer, message: "created" });
+      await this.lawyerService.createLawyer(lawyerData);
+      res.status(200).json({ message: "created" });
     } catch (error) {
       next(error);
     }
