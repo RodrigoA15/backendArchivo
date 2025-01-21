2072e3dbb763e24ebcd4525cfe753e53e973646e
RodrigoA15 <rapapamija@misena.edu.co>
2025-01-21 17:35:23 -0500

    Fix: Timestamps Evidence file Schema

--- src/schemas/EvidenceFile.schema.ts
+++ src/schemas/EvidenceFile.schema.ts
@@ -2,10 +2,15 @@ import { Schema, model } from "mongoose";
 import mongoose from "mongoose";
 import { EvidenceFile } from "../interfaces/evidenceFile.interface";
 
-const EvidenceFileSchema = new Schema<EvidenceFile>({
-  url_evidence: { type: String, required: true },
-  consecutive: { type: Number, required: true },
-  id_file: { type: mongoose.Schema.Types.ObjectId, required: true },
-});
+const EvidenceFileSchema = new Schema<EvidenceFile>(
+  {
+    url_evidence: { type: String, required: true },
+    consecutive: { type: Number, required: true },
+    id_file: { type: mongoose.Schema.Types.ObjectId, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default model<EvidenceFile>("EvidenceFile", EvidenceFileSchema);
