06147e3fa3fee4d8631ceb239340247d2966912e
RodrigoA15 <rapapamija@misena.edu.co>
2025-02-04 17:21:30 -0500

    Add: Field Dto Updatedfiles

--- src/dtos/UpdateStatus.dto.ts
+++ src/dtos/UpdateStatus.dto.ts
@@ -1,9 +1,13 @@
-import { IsMongoId, IsNotEmpty, IsBoolean } from "class-validator";
+import { IsMongoId, IsNotEmpty, IsBoolean, IsOptional } from "class-validator";
 
 export class UpdateStatusDto {
   @IsNotEmpty()
   @IsMongoId({ each: true })
   _id: string[];
+
+  @IsOptional()
+  @IsMongoId()
+  lawyer_id: string;
 
   @IsNotEmpty()
   @IsBoolean()
