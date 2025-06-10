23c37c6b595a633ef03696cd754a0fda63698d08
RodrigoA15 <rapapamija@misena.edu.co>
2025-06-10 11:41:01 -0500

    Add: Timestamps

--- src/schemas/resolutions.schema.ts
+++ src/schemas/resolutions.schema.ts
@@ -1,12 +1,17 @@
 import { model, Schema } from "mongoose";
 import { Resolutions } from "../interfaces/resolutions.interface";
 
-const ResolutionsSchema = new Schema<Resolutions>({
-  resolution_number: { type: String, required: true, unique: true },
-  resolution_date: { type: Date, required: true },
-  resolution_type: { type: String, required: true },
-  file_id: { type: Schema.Types.ObjectId, ref: "files" },
-  user_id: { type: Schema.Types.ObjectId, required: true },
-});
+const ResolutionsSchema = new Schema<Resolutions>(
+  {
+    resolution_number: { type: String, required: true, unique: true },
+    resolution_date: { type: Date, required: true },
+    resolution_type: { type: String, required: true },
+    file_id: { type: Schema.Types.ObjectId, ref: "files" },
+    user_id: { type: Schema.Types.ObjectId, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default model<Resolutions>("Resolutions", ResolutionsSchema);
